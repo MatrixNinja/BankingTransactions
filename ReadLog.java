21d2373a3999f9e6011af259ad5a4bba7445c3db
Emilio G. <hitsone.sccc@gmail.com>
2013-08-14 13:12:06 -0700

    Creates a transaction log
    When hitting the Submit button on the GUI, the amount entered is stored into the (TransactionLog.txt) file.

---
+++ ReadLog.java
@@ -0,0 +1,30 @@
+package com.egarduno.Banking;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReadLog {
+	private String path;
+	
+	public ReadLog(String path){
+		this.path = path;
+	}
+	
+	@SuppressWarnings("resource")
+	public String getText() throws IOException{
+		String content = "";
+		FileInputStream fstream = new FileInputStream(path);//reads the file
+		DataInputStream fileIn = new DataInputStream(fstream);
+		BufferedReader buffReader = new BufferedReader(new InputStreamReader(fileIn));//buffers it into memory
+		String stringIn;
+		
+		while((stringIn = buffReader.readLine()) != null){
+			content += (stringIn + "\n");
+		}
+		
+		return content;
+	}
+	
+}
