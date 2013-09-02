6894cf78dff59ed7debacb0bb36029ee637fed41
Emilio G. <hitsone.sccc@gmail.com>
2013-09-02 09:54:19 -0700

    Update Display.java

--- Display.java
+++ Display.java
@@ -3,8 +3,14 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.*;
-
 import javax.swing.*;
+
+/*__________________________________
+
+	Emilio Garduno
+	08/14/2013
+	Banking Transactions
+__________________________________*/
 
 public class Display {
 	//==========GUI
